6efdb2a8b923e2730a9b7e831ba95c475d3ba63b
Pradipta Nandi <nandip500@gmail.com>
2021-09-22 12:31:54 +0530

    completed

--- src/pages/plays.page.js
+++ src/pages/plays.page.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Poster from "../components/poster/poster.component";
+import Poster from "../components/poster/poster";
 import PlaysFilter from "../components/PlaysFilter/playsfilters.component";
 const Plays = () => {
 return (
 <>
+
+
+
 <div className="container mx-auto px-4">
     <div className="w-full lg:flex lg:flex-row-reverse">
         <div className="lg:w-8/12">
@@ -11,7 +14,7 @@ return (
         <div className="flex flex-wrap">
             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
                 <Poster
-                    src="korea.jpg"
+                    src="https://pbs.twimg.com/media/E_iswnwVUAYdoXC.jpg"
                     title="My Name is Jaan"
                     subtitle="Bengali ₹299"
                     />
@@ -36,21 +39,7 @@ return (
                     title="Tvk Cultural presents Ponniyin Selvan"
                     subtitle="Tamil ₹300"
                     />
-            </div>
-            <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                <Poster
-                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAyNiBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314527-exbxfrdxnw-portrait.jpg"
-                    title="TVK Presents Amarar Kalki in Sivagamiyin Sabatham"
-                    subtitle="Tamil ₹199"
-                    />
-            </div>
-            <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                <Poster
-                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxOCBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314524-zvbthgclqk-portrait.jpg"
-                    title="TVK Presents Amarar Kalki in Sivagamiyin Sabatham"
-                    subtitle="Tamil ₹199"
-                    />
-            </div>
+            </div>      
             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
                 <Poster
                     src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAyNSBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314535-rnjpjrfjrc-portrait.jpg"
@@ -63,13 +52,6 @@ return (
                     src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAyNiBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314536-rfdhbzbknd-portrait.jpg"
                     title="Tvk Cultural presents Ponniyin Selvan"
                     subtitle="Tamil ₹300"
-                    />
-            </div>
-            <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                <Poster
-                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOSBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314525-esuazfuvmt-portrait.jpg"
-                    title="TVK Presents Amarar Kalki in Sivagamiyin Sabatham"
-                    subtitle="Tamil ₹199"
                     />
             </div>
             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
